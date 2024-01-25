2b65d6da3b4ec37a52a6a53212ccccf72fa498b4
yms1997 <147358550+yms1997@users.noreply.github.com>
2024-01-25 18:41:03 +0900

    update

--- canvas.js
+++ canvas.js
@@ -1,5 +1,6 @@
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
+const gameBtn = document.querySelector('#start');
 
 // 기본셋팅
 let x = canvas.width / 2;
@@ -7,7 +8,7 @@ let y = canvas.height - 20;
 let ballRadius = 10;
 let dx = 1;
 let dy = -1;
-let ballSpeed = 1.5;
+let ballSpeed = 5;
 // paddle(밑에 움직이는 막대기같은거)
 const paddleHeight = 10;
 const paddleWidth = 75;
@@ -15,8 +16,8 @@ let paddleX = (canvas.width - paddleWidth) / 2;
 // key(event)
 let key = {"ArrowRight": false, "ArrowLeft": false};
 // 벽돌
-let brickRowCount = 3; // 열 수
-let brickColumnCount = 5; // 횡 수
+let brickRowCount = 1; // 열 수
+let brickColumnCount = 1; // 횡 수
 let brickWidth = 75; // 벽돌 너비
 let brickHeight = 20; // 벽돌 높이
 let brickPadding = 10; // 벽돌 사이 간격
@@ -28,10 +29,12 @@ let lives = 3;
 
 // 벽돌관리
 let bricks = [];
-for(let c = 0; c < brickColumnCount; c++){
-  bricks[c] = [];
-  for(let r = 0; r < brickRowCount; r++){
-    bricks[c][r] = {x : 0, y : 0, status : 1};
+function createbrick(){
+  for(let c = 0; c < brickColumnCount; c++){
+    bricks[c] = [];
+    for(let r = 0; r < brickRowCount; r++){
+      bricks[c][r] = {x : 0, y : 0, status : 1};
+    }
   }
 }
 
@@ -53,6 +56,7 @@ function mouseMoveHandler(event){
     paddleX = relativeX - paddleWidth / 2;
   }
 }
+
 
 function drawBricks(){
   for(let c = 0; c < brickColumnCount; c++){
@@ -87,11 +91,6 @@ function collisionDetction(){
         b.status = 0;
         score++;
       }
-      if(score === brickColumnCount * brickRowCount){
-        clearInterval(tmp);
-        alert("YOU WIN, CONGRATURATIONS!");
-        document.location.reload();
-      }
     }
   }
 }
@@ -120,25 +119,59 @@ function drawBall(){
   ctx.closePath();
 }
 function draw(){
+  gameBtn.style.display = 'none';
+  canvas.style.opacity = 1;
+  // canvas.style.cursor = "none";
   ctx.clearRect(0, 0, canvas.width, canvas.height)
+  createbrick();
   drawBricks();
   drawBall();
   drawPaddle();
   drawScore();
+  drawLives();
   collisionDetction();
 
   // 공튀기기 체크
   if(y + dy < ballRadius){
     dy = -dy;
   } else if(y + dy > canvas.height - ballRadius){ // 바닥에 닿았을때
-    alert("GAME OVER");
-    document.location.reload();
-    clearInterval(tmp);
+    lives--;
+    if(!lives){
+      drawLives();
+      alert("GAME OVER");
+      document.location.reload();
+    }
+    else{
+      x = canvas.width / 2;
+      y = canvas.height - 30;
+      dx = 1;
+      dy = -1;
+      paddleX = (canvas.width - paddleWidth) / 2;
+    }
   } else if(y + dy > canvas.height - ballRadius - paddleHeight && x + dx > paddleX && x + dx < paddleX + paddleWidth){
     dy = -dy; // 페달에 닿앗을때 다시 팅기기
   }
   if(x + dx > canvas.width - ballRadius || x + dx < ballRadius){
     dx = -dx;
+  }
+
+  if(score === brickColumnCount * brickRowCount){
+    alert("YOU WIN, CONGRATURATIONS!");
+    clearInterval(game);
+    // document.location.reload();
+    canvas.style.opacity = 0;
+    gameBtn.style.display = "block";
+    gameBtn.innerText = "다시하기";
+    gameBtn.addEventListener('click', () => {
+      score = 0;
+      lives = 3;
+      x = canvas.width / 2;
+      y = canvas.height - 30;
+      dx = 1;
+      dy = -1;
+      paddleX = (canvas.width - paddleWidth) / 2;
+      bricks = [];
+    })
   }
 
   // 키 체크
@@ -155,4 +188,8 @@ document.addEventListener("keydown", e => keyHandler(e, true));
 document.addEventListener("keyup", e => keyHandler(e, false));
 document.addEventListener("mousemove", mouseMoveHandler, false);
 
-let tmp = setInterval(draw, 10);
+let game;
+
+gameBtn.addEventListener('click', e => {
+  game = setInterval(draw, 10);
+})
