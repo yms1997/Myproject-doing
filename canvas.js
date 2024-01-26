fb3363a892c7afdee5e57356968117a2674bb2f3
yms1997 <147358550+yms1997@users.noreply.github.com>
2024-01-26 18:40:22 +0900

    update

--- canvas.js
+++ canvas.js
@@ -8,28 +8,96 @@ let y = canvas.height - 20;
 let ballRadius = 10;
 let dx = 1;
 let dy = -1;
-let ballSpeed = 5;
+let ballSpeed = 1;
 // paddle(밑에 움직이는 막대기같은거)
-const paddleHeight = 10;
-const paddleWidth = 75;
+let paddleHeight = 10;
+let paddleWidth = 75;
 let paddleX = (canvas.width - paddleWidth) / 2;
 // key(event)
 let key = {"ArrowRight": false, "ArrowLeft": false};
 // 벽돌
-let brickRowCount = 1; // 열 수
-let brickColumnCount = 1; // 횡 수
+let brickRowCount = 0; // 열 수
+let brickColumnCount = 0; // 횡 수
 let brickWidth = 75; // 벽돌 너비
 let brickHeight = 20; // 벽돌 높이
 let brickPadding = 10; // 벽돌 사이 간격
 let brickOffsetTop = 30; // 캔버스 모서리 안닿게
-let brinckoffsetLeft = 30; // 캔버스 모서리 안닿게
+let brinckoffsetLeft = 20; // 캔버스 모서리 안닿게
 // 스코어 및 생명
 let score = 0;
 let lives = 3;
-
 // 벽돌관리
 let bricks = [];
-function createbrick(){
+// 타이머 
+let game = null; 
+
+// 난이도
+let difficulty = "easy";
+document.getElementById('difficulty').addEventListener('change', function(event) {
+  difficulty = event.target.value;
+  console.log(difficulty)
+  applydifficulty();
+})
+
+function applydifficulty(){
+  switch(difficulty){
+    case "easy":
+      // 벽돌
+      brickRowCount = 1;
+      brickColumnCount = 1;
+      // 생명
+      lives = 5;
+      // 페달
+      paddleWidth = 200;
+      // 공
+      ballSpeed = 1;
+      break;
+    case "hard":
+      // 벽돌
+      brickRowCount = 7;
+      brickColumnCount = 9;
+      // 생명
+      lives = 1;
+      // 페달
+      paddleWidth = 50;
+      // 공
+      ballSpeed = 3;
+      break;
+    default:
+      // 벽돌
+      brickRowCount = 5;
+      brickColumnCount = 9;
+      ballSpeed = 1.5;
+  }
+  
+  createBricks(brickColumnCount , brickRowCount);
+}
+
+function reset(){
+  x = canvas.width / 2;
+  y = canvas.height - 20;
+  ballRadius = 10;
+  dx = 1;
+  dy = -1;
+  ballSpeed = 1;
+  paddleHeight = 10;
+  paddleWidth = 75;
+  paddleX = (canvas.width - paddleWidth) / 2;
+  key = {"ArrowRight": false, "ArrowLeft": false};
+  brickRowCount = 0;
+  brickColumnCount = 0;
+  brickWidth = 75;
+  brickHeight = 20;
+  brickPadding = 10;
+  brickOffsetTop = 30;
+  brinckoffsetLeft = 20;
+  score = 0;
+  lives = 3;
+  bricks = [];
+  game = null; 
+}
+
+function createBricks(brickColumnCount , brickRowCount){
   for(let c = 0; c < brickColumnCount; c++){
     bricks[c] = [];
     for(let r = 0; r < brickRowCount; r++){
@@ -61,18 +129,17 @@ function mouseMoveHandler(event){
 function drawBricks(){
   for(let c = 0; c < brickColumnCount; c++){
     for(let r = 0; r < brickRowCount; r++){
-      if(bricks[c][r].status === 0){
-        continue;
+      if(bricks[c][r].status === 1){
+        let brickX = (c * (brickWidth + brickPadding)) + brinckoffsetLeft;
+        let brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+        bricks[c][r].x = brickX;
+        bricks[c][r].y = brickY;
+        ctx.beginPath();
+        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+        ctx.fillStyle = '#0095DD';
+        ctx.fill();
+        ctx.closePath();
       }
-      let brickX = (c * (brickWidth + brickPadding)) + brinckoffsetLeft;
-      let brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
-      bricks[c][r].x = brickX;
-      bricks[c][r].y = brickY;
-      ctx.beginPath();
-      ctx.rect(brickX, brickY, brickWidth, brickHeight);
-      ctx.fillStyle = '#0095DD';
-      ctx.fill();
-      ctx.closePath();
     }
   }
 }
@@ -123,7 +190,6 @@ function draw(){
   canvas.style.opacity = 1;
   // canvas.style.cursor = "none";
   ctx.clearRect(0, 0, canvas.width, canvas.height)
-  createbrick();
   drawBricks();
   drawBall();
   drawPaddle();
@@ -163,14 +229,8 @@ function draw(){
     gameBtn.style.display = "block";
     gameBtn.innerText = "다시하기";
     gameBtn.addEventListener('click', () => {
-      score = 0;
-      lives = 3;
-      x = canvas.width / 2;
-      y = canvas.height - 30;
-      dx = 1;
-      dy = -1;
-      paddleX = (canvas.width - paddleWidth) / 2;
-      bricks = [];
+      reset();
+      applydifficulty();
     })
   }
 
@@ -188,8 +248,7 @@ document.addEventListener("keydown", e => keyHandler(e, true));
 document.addEventListener("keyup", e => keyHandler(e, false));
 document.addEventListener("mousemove", mouseMoveHandler, false);
 
-let game;
-
 gameBtn.addEventListener('click', e => {
   game = setInterval(draw, 10);
+  applydifficulty();
 })
